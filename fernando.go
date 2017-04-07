018ad0912377201a404155ecd6904e3b910d7458
Isabelle Monfort <ysabelmonfort@gmail.com>
2017-04-07 12:10:02 +0200

    Change image and style in html and css files

--- fernando.go
+++ fernando.go
@@ -1,14 +1,15 @@
 package main
 
-import (
+import(
     "fmt"
     "io/ioutil"
     "net/http"
 )
 
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    hereweare := r.URL.Path[len("/"):]
-    content, err := ioutil.ReadFile("web/" + hereweare)
+    fernando := r.URL.Path[len("/"):]
+    content, err := ioutil.ReadFile("web/" + fernando)
     if err != nil {
         fmt.Fprintf(w, "Error : %s", err);
     } else {
